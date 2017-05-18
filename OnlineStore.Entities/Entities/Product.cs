92c665a8a88f96532b6e32c0a85685e706430428
artem <demfoss@gmail.com>
2017-05-18 22:51:28 +0300

    Final Models

--- OnlineStore.Entities/Entities/Product.cs
+++ OnlineStore.Entities/Entities/Product.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace OnlineStore.Domain.Entities
 {
     public class Product
     {
+        [Key]
+        [HiddenInput(DisplayValue = false)]
+       
         public int Id { get; set; }
+        [Required(ErrorMessage ="Please add Name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please add Description")]
         public string Description { get; set; }
+        [ForeignKey("Store")]
+        [Required(ErrorMessage = "Please add store")]
+    
         public int IdStore { get; set; }
         public Store Store { get; set; }
     }
--- OnlineStore.Entities/Entities/Store.cs
+++ OnlineStore.Entities/Entities/Store.cs
@@ -4,15 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace OnlineStore.Domain.Entities
 {
     public class Store
     {
+       
+        [Key]
         [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please add Name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please add Adress")]
         public string Adress { get; set; }
-        public TimeSpan WorkingHours { get; set; }
+        [Required(ErrorMessage = "Please add work time")]
+        public TimeSpan WorkTime { get; set; }
     }
 }
