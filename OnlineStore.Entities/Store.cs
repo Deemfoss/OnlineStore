84e594f8c4e2cc984068fa13119b5e4a1c10b39d
artem <demfoss@gmail.com>
2017-05-18 22:27:44 +0300

    Model

---
+++ OnlineStore.Entities/Store.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Domain.Entities
+{
+    public class Store
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public TimeSpan WorkingHours { get; set; }
+    }
+}
